ad8b64242ae57ef33ddcf9fde2796f331ef1aa8d
Pavel <azazasdfg.com@gmail.com>
2023-07-10 23:24:11 +0300

    Add prisma adapter to next-auth options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,17 @@
+import { prisma } from "@/lib/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import nextAuth, { NextAuthOptions } from "next-auth"
+import type { Adapter } from "next-auth/adapters"
 import GitHubProveder from "next-auth/providers/github"
 
 const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GitHubProveder({
       clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string
     })
-  ],
-  secret: process.env.NEXTAUTH_SECRET as string
+  ]
 }
 
 const handler = nextAuth(authOptions)
