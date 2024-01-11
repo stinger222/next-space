7b03e8939f0bf2710af99270074a7bb951860dbf
Pavel <azazasdfg.com@gmail.com>
2024-01-12 02:29:00 +0300

    Add revalidate for users list page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,10 +6,11 @@ export const metadata = {
   description: "List of all users registered in NextSpace application"
 }
 
+export const revalidate = 0
+
 const Users = async () => {
 
   const users = await prisma.user.findMany()
-
   return (
     <div className="flex flex-wrap justify-center gap-x-10 gap-y-5">
       {users.map((user) => (
